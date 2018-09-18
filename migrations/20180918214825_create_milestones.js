f877ffd8daf16a220b97b0eb41476b2acd2f163e
Thofeeq <thofeeqm@gmail.com>
2018-09-18 23:10:28 +0000

    Added another field (f.key - famous_people_id) to the milestone table[migration - up]

--- migrations/20180918214825_create_milestones.js
+++ migrations/20180918214825_create_milestones.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('milestones',function(t){
     t.increments('id');
-    t.string('description');
+    t.string('description',255);
     t.date('date_achieved');
   });
 };
