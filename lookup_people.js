a26eeddf1b96857fcd03ecd184bf2a9403c8076d
Thofeeq <thofeeqm@gmail.com>
2018-09-18 19:39:59 +0000

    Adding some docs to the code

--- lookup_people.js
+++ lookup_people.js
@@ -1,7 +1,8 @@
+// Required setups
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
-const arg = process.argv.slice(2).join("");
-const client = new pg.Client({
+const arg = process.argv.slice(2).join(""); //getting argument from command line (Name of the person being looked up)
+const client = new pg.Client({ //passing the credentials/required info to access the db
   user     : settings.user,
   password : settings.password,
   database : settings.database,
@@ -10,20 +11,24 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-client.connect((err) => {
+//connecting to the db
+client.connect((err) => {       
   
   if (err) {
     return console.error("Connection Error", err);
-  }                                                             
+  }
+  //selecting the first name to query against the argument provided                                                             
   client.query("SELECT * FROM famous_people WHERE first_name LIKE $1::text",[arg], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
+    //looping through the records returned
     console.log("Searching...");
     console.log(`Found ${result.rows.length} person(s) by the name ${arg}`);
     for(i = 0; i < result.rows.length; i++){
       console.log(`${i+1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born '${result.rows[i].birthdate}'`); 
     }
+    //closing the db
     client.end();
   });
-});
+});  
