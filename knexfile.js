e1ab9ea6ce37cc9e8ec3644770887597adf3d662
Thofeeq <thofeeqm@gmail.com>
2018-09-18 22:55:48 +0000

    Added a new table 'milestones' to the db

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+const settings = require("./settings")
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  staging: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ migrations/20180918214825_create_milestones.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('milestones',function(t){
+    t.increments('id');
+    t.string('description');
+    t.date('date_achieved');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('milestones');
+};
